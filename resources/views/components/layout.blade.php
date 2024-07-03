b8be299e672540245773a7ed3d54c566d464cbef
AngelPhenix <breathstylebro@gmail.com>
2024-07-03 13:49:44 +0200

    Added new layout view

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JobPositions</title>
+</head>
+<body>
+    <div>
+        <nav>
+
+        </nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+
+    </div>
+</body>
+</html>
