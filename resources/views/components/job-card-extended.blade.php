4ced0e26f29af65e5f7d44c2f0275ab3ebb8ed5a
AngelPhenix <breathstylebro@gmail.com>
2024-07-03 17:39:56 +0200

    Added picsum to generate random pictures for jobs as a test

--- resources/views/components/job-card-extended.blade.php
+++ resources/views/components/job-card-extended.blade.php
@@ -1,10 +1,10 @@
 <div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
     <div>
-        <img src="http://placehold.co/90x90" alt="logo" class="rounded-xl">
+        <img src="http://picsum.photos/seed/{{ rand(0, 150) }}/90/90" alt="logo" class="rounded-xl">
     </div>
 
     <div class="flex-1">
-        <a href="#" class="self-start text-sm">GovExec</a>
+        <a href="#" class="self-start text-sm text-gray-400">GovExec</a>
         <h3>Web Developer</h3>
         <p>Full Time - From 40,000€</p>
     </div>
