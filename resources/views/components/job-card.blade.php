b8ec29a3aa4a1687fe24d739075b423f733a7186
AngelPhenix <breathstylebro@gmail.com>
2024-07-03 14:26:55 +0200

    Added new job-card component to include in layout

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">GovExec</div>
+
+    <div class="py-8 font-bold">
+        <h3>Web Developer</h3>
+        <p>Full Time - From 40,000€</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+            <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+            <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+        </div>
+        
+        <img src="http://placehold.co/42x42" alt="logo" class="rounded-xl">
+    </div>
+</div>
