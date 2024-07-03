c2519942a6c8b20aa7059c93127a2dda889510a0
AngelPhenix <breathstylebro@gmail.com>
2024-07-03 17:36:34 +0200

    Added job-card-extended component

---
+++ resources/views/components/job-card-extended.blade.php
@@ -0,0 +1,17 @@
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+    <div>
+        <img src="http://placehold.co/90x90" alt="logo" class="rounded-xl">
+    </div>
+
+    <div class="flex-1">
+        <a href="#" class="self-start text-sm">GovExec</a>
+        <h3>Web Developer</h3>
+        <p>Full Time - From 40,000€</p>
+    </div>
+
+    <div>
+        <x-tag>Frontend</x-tag>
+        <x-tag>Backend</x-tag>
+        <x-tag>Fullstack</x-tag>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,6 +29,12 @@
 
         <section>
         <x-section-heading>Recent Jobs</x-section-heading>
+
+        <div class="mt-6 space-y-6">
+            <x-job-card-extended />
+            <x-job-card-extended />
+            <x-job-card-extended />
+        </div>
         </section>
     </div>
 </x-layout>
