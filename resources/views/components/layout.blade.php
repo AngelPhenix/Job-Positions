7a1f3a443540787b20f97341c9fd65f807165651
AngelPhenix <breathstylebro@gmail.com>
2024-07-03 13:56:46 +0200

    Added logo SVG with Vite manifest

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JobPositions</title>
+    @vite(['resources/js/app.js'])
 </head>
 <body>
     <div>
         <nav>
-
+            <div>
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="logo">
+                </a>
+            </div>
+            <div>
+                links
+            </div>
+            <div>
+                post a job 
+            </div>
         </nav>
 
         <main>
