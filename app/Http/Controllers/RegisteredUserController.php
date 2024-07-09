cb8acd1c3a1e93bf7455babde594b2841ea0ffc9
AngelPhenix <breathstylebro@gmail.com>
2024-07-10 00:30:15 +0200

    Finishing storing up info in DB

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Validation\Rules\Password;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisteredUserController extends Controller
 {
@@ -27,7 +32,20 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $userAttributes = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'confirmed', Password::min(6)]
+        ]);
+
+        $employerAttributes = $request->validate([
+            'name' => ['required'],
+            'logo' => ['required', File::types(['png', 'jpg', 'webp'])]
+        ]);
+
+        $user = User::create($userAttributes);
+
+        Auth::login($user);
     }
 
     /**
