0d37df6ebd477c66b3e2c4a60c1b18383a4fafa8
AngelPhenix <breathstylebro@gmail.com>
2024-07-09 22:54:06 +0200

    Fixed label component

--- resources/views/components/forms/label.blade.php
+++ resources/views/components/forms/label.blade.php
@@ -1,3 +1,6 @@
 @props(['label', 'name'])
 
-<label for="{{name}}" name="{{$name}}">{{$label}}</label>
+<div class="inline-flex items-center gap-x-2">
+    <span class="w-2 h-2 bg-white inline-block"></span>
+    <label class="font-bold" for="{{name}}">{{$label}}</label>
+</div>
