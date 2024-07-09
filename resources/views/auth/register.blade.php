fbc3c593255048218625ec57cbfb185b10d9194f
AngelPhenix <breathstylebro@gmail.com>
2024-07-10 00:15:56 +0200

    Added input component

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
     <x-page-heading>Register</x-page-heading>
+
+    <x-forms.form mehotd="POST" action="/register">
+        <x-forms.input label="Your name" name="name"/>
+    </x-forms.form>
 </x-layout>
