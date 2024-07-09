9550740a865632b27f6b03d3d610556b09660df0
AngelPhenix <breathstylebro@gmail.com>
2024-07-10 00:23:54 +0200

    Finished adding fields to register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,5 +3,15 @@
 
     <x-forms.form mehotd="POST" action="/register">
         <x-forms.input label="Your name" name="name"/>
+        <x-forms.input label="Email" name="email" type="email"/>
+        <x-forms.input label="Password" name="password" type="password"/>
+        <x-forms.input label="Password Confirmation" name="password_confirmation" type="password"/>
+
+        <x-forms.divider/>
+
+        <x-forms.input label="Employer Name" name="employer"/>
+        <x-forms.input label="Employer Logo" name="logo" type="file"/>
+
+        <x-forms.button>Create an account</x-forms.button>
     </x-forms.form>
 </x-layout>
