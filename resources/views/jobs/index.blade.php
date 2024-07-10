4cc7d05820bf00234e5e9051b07b308cb927b4b6
AngelPhenix <breathstylebro@gmail.com>
2024-07-11 01:18:41 +0200

    Changed job searching input with form component

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -3,9 +3,9 @@
         <section class="text-center pt-5">
             <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
 
-            <form action="" class="mt-6" >
-                <input class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full max-w-xl" type="text" name="" id="" placeholder="I'm looking for..">
-            </form>
+            <x-forms.form action="/search" class="mt-6" >
+                <x-forms.input :label="false" name="query" placeholder="I'm looking for.." />
+            </x-forms.form>
         </section>
 
 
