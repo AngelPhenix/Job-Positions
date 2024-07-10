86b1bf8cd86170f3ab2b0d6a269dac60098fefd1
AngelPhenix <breathstylebro@gmail.com>
2024-07-11 01:24:57 +0200

    Dealt with title search to be sent to results view

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     public function __invoke()
     {
-        dd(request('query'));
+        $jobs = Job::where('title', 'LIKE', '%'.request('query').'%')->get();
+
+        return view('results', ['jobs' => $jobs];)
     }
 }
