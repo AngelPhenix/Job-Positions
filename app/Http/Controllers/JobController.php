0aa3092685b3bf00827b51bcfe08a07d541d733b
AngelPhenix <breathstylebro@gmail.com>
2024-07-12 01:24:11 +0200

    Fixed order by for jobs on main page

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -18,11 +18,11 @@ class JobController extends Controller
      */
     public function index()
     {
-        $jobs = Job::all()->groupBy('featured');
+        $jobs = Job::latest()->get()->groupBy('featured');
 
         return view('jobs.index', [
-            'featuredJobs' => $jobs[1],
             'jobs' =>  $jobs[0],
+            'featuredJobs' => $jobs[1],
             'tags' => Tag::all()
         ]);
     }
